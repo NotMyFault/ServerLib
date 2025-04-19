2b846c761287665d908c49601a36e82ae102a68b
Alexander Brandes <mc.cache@web.de>
2025-04-19 20:29:06 +0200

    Try new release plugin
    Signed-off-by: Alexander Brandes <mc.cache@web.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
+import com.vanniktech.maven.publish.SonatypeHost
 import java.net.URI
 
 plugins {
     java
-    `maven-publish`
     signing
 
-    alias(libs.plugins.nexus)
+    alias(libs.plugins.publish)
 }
 
 group = "dev.notmyfault.serverlib"
-version = "2.3.7-SNAPSHOT"
+version = "2.3.7"
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
@@ -31,31 +31,10 @@ dependencies {
     compileOnly(libs.paper)
 }
 
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
-
 tasks {
-
-    compileJava {
-        options.compilerArgs.addAll(arrayOf("-Xmaxerrs", "1000"))
-        options.compilerArgs.add("-Xlint:all")
-        for (disabledLint in arrayOf("processing", "path", "fallthrough", "serial"))
-            options.compilerArgs.add("-Xlint:$disabledLint")
-        options.isDeprecation = true
-        options.encoding = "UTF-8"
-    }
 
     javadoc {
         val opt = options as StandardJavadocDocletOptions
-        opt.addStringOption("Xdoclint:none", "-quiet")
-        opt.tags(
-                "apiNote:a:API Note:",
-                "implSpec:a:Implementation Requirements:",
-                "implNote:a:Implementation Note:"
-        )
-        opt.addBooleanOption("html5", true)
         opt.noTimestamp()
     }
 
@@ -77,61 +56,48 @@ if (!project.hasProperty("skip.signing")) {
     }
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
+mavenPublishing {
+    coordinates(
+            groupId = "$group",
+            artifactId = project.name,
+            version = "${project.version}",
+    )
 
-            pom {
+    pom {
+        name.set(project.name)
+        description.set("ServerLib is a tool that attempts to detect unsafe implementations of the Bukkit API.")
+        url.set("https://github.com/NotMyFault/ServerLib")
 
-                name.set(project.name + " " + project.version)
-                description.set("ServerLib is a tool that attempts to detect unsafe implementations of the Bukkit API.")
-                url.set("https://github.com/NotMyFault/ServerLib")
-
-                licenses {
-                    license {
-                        name.set("MIT")
-                        url.set("https://opensource.org/licenses/mit")
-                        distribution.set("repo")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("NotMyFault")
-                        name.set("NotMyFault")
-                        email.set("contact(at)notmyfault.dev")
-                        organization.set("IntellectualSites")
-                        organizationUrl.set("https://github.com/IntellectualSites/")
-                    }
-
-                    developer {
-                        id.set("Citymonstret")
-                        name.set("Alexander Söderberg")
-                    }
-                }
-
-                scm {
-                    url.set("https://github.com/NotMyFault/ServerLib")
-                    connection.set("scm:git:https://github.com/NotMyFault/ServerLib.git")
-                    developerConnection.set("scm:git:git@github.com:NotMyFault/ServerLib.git")
-                    tag.set(project.version.toString())
-                }
-
-                issueManagement {
-                    system.set("GitHub")
-                    url.set("https://github.com/NotMyFault/ServerLib/issues")
-                }
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/mit")
+                distribution.set("repo")
             }
         }
-    }
-}
 
-nexusPublishing {
-    this.repositories {
-        sonatype {
-            nexusUrl.set(URI.create("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(URI.create("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        developers {
+            developer {
+                id.set("NotMyFault")
+                name.set("NotMyFault")
+                email.set("contact(at)notmyfault.dev")
+                organization.set("IntellectualSites")
+                organizationUrl.set("https://github.com/IntellectualSites/")
+            }
         }
+
+        scm {
+            url.set("https://github.com/NotMyFault/ServerLib")
+            connection.set("scm:git:https://github.com/NotMyFault/ServerLib.git")
+            developerConnection.set("scm:git:git@github.com:NotMyFault/ServerLib.git")
+            tag.set(project.version.toString())
+        }
+
+        issueManagement {
+            system.set("GitHub")
+            url.set("https://github.com/NotMyFault/ServerLib/issues")
+        }
+
+        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
     }
 }
